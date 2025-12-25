e67cfba5e51ade5e4db3569f6985f0363a447736
Vituinho <trabalhovictor531@gmail.com>
2025-12-25 20:55:52 -0300

    feat: Classes

---
+++ classes.js
@@ -0,0 +1,21 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar() {
+        console.log(`${this.nome} Está falando`)
+    }
+}
+
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+    console.log(`${this.nome} Está falando`)
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda');
--- newForeach.js
+++ newForeach.js
@@ -6,3 +6,5 @@ a1.forEach(valor => {
 });
 
 console.log(total)
+
+// Bem importante lembrar, que o Foreach do JS é diferente dos outros, ele usa a1.forEach sendo com um ponto e depois o foreach, bem interessante, porem diferente, e ele vem com valor, indice e array, q nem o filter, map e reduce
